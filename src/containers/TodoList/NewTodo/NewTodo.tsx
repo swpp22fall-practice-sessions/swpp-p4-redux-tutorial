e4279cf2441f4a508bb4768ea3f1505e64ad3f81
ccdc-git <titieiti.com@gmail.com>
2022-09-25 23:52:23 +0900

    :sparkles: Add NewTodo action

--- src/containers/TodoList/NewTodo/NewTodo.tsx
+++ src/containers/TodoList/NewTodo/NewTodo.tsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
 // import { useNavigate } from "react-router-dom";
-import { todoActions } from "../../../store/slices/todo";
+import { AppDispatch } from "../../../store";
+import { postTodo } from "../../../store/slices/todo";
 import "./NewTodo.css";
 
 export default function NewTodo() {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [submitted, setSubmitted] = useState<boolean>(false);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   // const navigate = useNavigate()
   // const postTodoHandler = () => {
@@ -19,10 +20,14 @@ export default function NewTodo() {
   //   navigate('/todos')
   // };
 
-  const postTodoHandler = () => {
+  const postTodoHandler = async () => {
     const data = { title: title, content: content };
-    dispatch(todoActions.addTodo(data));
-    setSubmitted(true);
+    const result = await dispatch(postTodo(data));
+    if (result.type === `${postTodo.typePrefix}/fulfilled`) {
+      setSubmitted(true);
+    } else {
+      alert("Error on post Todo");
+    }
   };
 
   if (submitted) {
