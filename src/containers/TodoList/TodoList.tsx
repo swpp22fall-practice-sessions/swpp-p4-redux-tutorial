9d3413c7f62ff3939cbe5408e135c2862a37e095
ccdc-git <titieiti.com@gmail.com>
2022-09-25 23:52:23 +0900

    :sparkles: Modify TodoDetail as page

--- src/containers/TodoList/TodoList.tsx
+++ src/containers/TodoList/TodoList.tsx
@@ -1,8 +1,6 @@
-import { useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import Todo from "../../components/Todo/Todo";
-import TodoDetail from "../../components/TodoDetail/TodoDetail";
 import { selectTodo, todoActions } from "../../store/slices/todo";
 import "./TodoList.css";
 
@@ -13,25 +11,15 @@ interface IProps {
 type TodoType = { id: number; title: string; content: string; done: boolean };
 
 export default function TodoList(props: IProps) {
+  const navigate = useNavigate();
   const { title } = props;
-  const [selectedTodo, setSelectedTodo] = useState<TodoType | null>(null);
 
   const todoState = useSelector(selectTodo);
   const dispatch = useDispatch();
 
   const clickTodoHandler = (td: TodoType) => {
-    if (selectedTodo === td) {
-      setSelectedTodo(null);
-    } else {
-      setSelectedTodo(td);
-    }
+    navigate("/todos/" + td.id);
   };
-
-  const todoDetail = useMemo(() => {
-    return selectedTodo ? (
-      <TodoDetail title={selectedTodo.title} content={selectedTodo.content} />
-    ) : null;
-  }, [selectedTodo]);
 
   return (
     <div className="TodoList">
@@ -44,12 +32,15 @@ export default function TodoList(props: IProps) {
               title={td.title}
               done={td.done}
               clickDetail={() => clickTodoHandler(td)}
-              clickDone={() => dispatch(todoActions.toggleDone({targetId: td.id}))}
-              clickDelete={() => dispatch(todoActions.deleteTodo({targetId: td.id}))}
+              clickDone={() =>
+                dispatch(todoActions.toggleDone({ targetId: td.id }))
+              }
+              clickDelete={() =>
+                dispatch(todoActions.deleteTodo({ targetId: td.id }))
+              }
             />
           );
         })}
-        {todoDetail}
         <NavLink to="/new-todo">New Todo</NavLink>
       </div>
     </div>
