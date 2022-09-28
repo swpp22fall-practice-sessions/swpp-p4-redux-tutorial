5a625427069a7879027d70900b29f5f4d1cbf346
rickyini3028 <rickyini3028@naver.com>
2022-09-28 20:34:50 +0900

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import TodoList from "./containers/TodoList/TodoList"; // can omit.js
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NewTodo from "./containers/TodoList/NewTodo/NewTodo";
 import TodoDetail from "./components/TodoDetail/TodoDetail";
-import { Provider } from "react-redux";
-import { store } from "./store";
 
 function App() {
   return (
@@ -18,18 +16,10 @@ function App() {
           <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
       </BrowserRouter>
-
     </div>
   );
 }
 
-
-
 export default App;
 
 <div className="TodoList"></div>;
-/* actually, it uses className, not class to avoid collision btw JS class
- * this syntax is compiled to React.createElement(‘div’, {className: ‘TodoList’})
- * by React Transpiler.
- * );
- */
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./slices/todo";
 
 export const store = configureStore({
-    reducer: {
-      todo: todoReducer,
-    },
-   });
-   
-   export type RootState = ReturnType<typeof store.getState>;
-   export type AppDispatch = typeof store.dispatch;
+  reducer: {
+    todo: todoReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
