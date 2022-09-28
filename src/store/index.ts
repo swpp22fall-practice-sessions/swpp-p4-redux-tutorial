e0edf08fc4f6928fe14b7312a0821bc510474d6a
EngineerA <engineerA314@github.com>
2022-09-29 00:48:10 +0900

    practice 4 finish

---
+++ src/store/index.ts
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./slices/todo";
+
+export const store = configureStore({ reducer: { todo: todoReducer } }); // TODO
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
