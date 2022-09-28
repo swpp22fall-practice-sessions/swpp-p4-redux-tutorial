d7984d96e4546b885ded762debfbb25efa6be3ca
MinJune Kim <qria@qria.net>
2022-09-29 08:11:33 +0900

    practice done

--- src/containers/TodoList/NewTodo/NewTodo.tsx
+++ src/containers/TodoList/NewTodo/NewTodo.tsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
+import { AppDispatch } from "../../../store";
+import { postTodo } from "../../../store/slices/todo";
 import "./NewTodo.css";
 
 export default function NewTodo() {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [submitted, setSubmitted] = useState<boolean>(false);
+  const dispatch = useDispatch<AppDispatch>();
 
-  // const navigate = useNavigate()
-  // const postTodoHandler = () => {
-  //   const data = { title: title, content: content };
-  //   alert("Submitted\n" + data.title + "\n" + data.content);
-  //   setSubmitted(true);
-  //   navigate('/todos')
-  // };
 
-  const postTodoHandler = () => {
+  const postTodoHandler = async () => {
     const data = { title: title, content: content };
-    alert("Submitted\n" + data.title + "\n" + data.content);
-    setSubmitted(true);
+    const result = await dispatch(postTodo(data));
+    if (result.type === `${postTodo.typePrefix}/fulfilled`) {
+      setSubmitted(true);
+    } else {
+      alert("Error on post Todo");
+    }
   };
 
   if (submitted) {
