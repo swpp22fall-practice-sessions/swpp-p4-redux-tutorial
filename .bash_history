8f3421df5e056b2adb2f4e6f6ee2801dc9cf9214
연태영 <dusxodud98@snu.ac.kr>
2022-09-28 20:44:41 +0900

    updated files

---
+++ .bash_history
@@ -0,0 +1,33 @@
+ls
+cd ..
+ls
+cd home
+ls
+ls
+cd ..
+ls
+cd root
+ls
+cd src
+ls
+cd ..
+ls
+yarn install
+yarn start
+ls
+yarn install
+yarn add redux react-redux @reduxjs/toolkit
+node redux-basics.js
+node redux-basics.js
+ls
+yarn start
+ls
+yarn install
+ls
+yarn start
+ls
+yarn install
+ls
+yarn build
+yarn start
+yarn start
--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import TodoList from "./containers/TodoList/TodoList"; // can omit.js
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NewTodo from "./containers/TodoList/NewTodo/NewTodo";
 import TodoDetail from "./components/TodoDetail/TodoDetail";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
+  
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/todos" element={<TodoList title={"My TODOs!"} />} />
-          <Route path="/todos/:id" element={<TodoDetail />} />
+          {/* <Route path="/todos/:id" element={<TodoDetail />} /> */}
           <Route path="/new-todo" element={<NewTodo />} />
           <Route path="/" element={<Navigate replace to={"/todos"} />} />
           <Route path="*" element={<h1>Not Found</h1>} />
