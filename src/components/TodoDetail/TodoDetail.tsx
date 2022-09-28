16eef974561c202f2cfe17c6abb1a9b0b4d6ed33
askrid <joonwoo3023@gmail.com>
2022-09-28 19:52:56 +0900

    implement Todo app without server

--- src/components/TodoDetail/TodoDetail.tsx
+++ src/components/TodoDetail/TodoDetail.tsx
@@ -1,20 +1,27 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import { selectTodo, todoActions } from "../../store/slices/todo";
 import "./TodoDetail.css";
 
-type Props = {
-  title: string;
-  content: string;
-};
+const TodoDetail = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const todoState = useSelector(selectTodo);
 
-const TodoDetail = (props: Props) => {
+  useEffect(() => {
+    dispatch(todoActions.getTodo({ targetId: Number(id) }));
+  }, [dispatch, id]);
+
   return (
     <div className="TodoDetail">
       <div className="row">
         <div className="left">Name:</div>
-        <div className="right">{props.title}</div>
+        <div className="right">{todoState.selectedTodo?.title}</div>
       </div>
       <div className="row">
         <div className="left">Content:</div>
-        <div className="right">{props.content}</div>
+        <div className="right">{todoState.selectedTodo?.content}</div>
       </div>
     </div>
   );
