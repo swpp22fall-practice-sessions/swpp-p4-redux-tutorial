e5b056630036096f5857209667d5419dbad99903
Donghyun Koh <andy1902@snu.ac.kr>
2022-09-29 02:40:08 +0900

    Finish Lab

--- src/containers/TodoList/TodoList.tsx
+++ src/containers/TodoList/TodoList.tsx
@@ -1,56 +1,60 @@
-import { useMemo, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { useEffect, useMemo, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import Todo from "../../components/Todo/Todo";
 import TodoDetail from "../../components/TodoDetail/TodoDetail";
 import "./TodoList.css";
+import { useDispatch, useSelector } from "react-redux";
+import { selectTodo, todoActions, fetchTodos, deleteTodo, toggleDone } from "../../store/slices/todo";
+import { AppDispatch } from "../../store";
 
 interface IProps {
-  title: string;
+	title: string;
 }
 
 type TodoType = { id: number; title: string; content: string; done: boolean };
 
 export default function TodoList(props: IProps) {
-  const { title } = props;
-  const [selectedTodo, setSelectedTodo] = useState<TodoType | null>(null);
+	const { title } = props;
+	const [selectedTodo, setSelectedTodo] = useState<TodoType | null>(null);
 
-  const [todos, setTodos] = useState<TodoType[]>([
-    { id: 1, title: "SWPP", content: "take swpp class", done: true },
-    { id: 2, title: "Movie", content: "watch movie", done: false },
-    { id: 3, title: "Dinner", content: "eat dinner", done: false },
-  ]);
+	const todoState = useSelector(selectTodo);
+	const dispatch = useDispatch<AppDispatch>();
 
-  const clickTodoHandler = (td: TodoType) => {
-    if (selectedTodo === td) {
-      setSelectedTodo(null);
-    } else {
-      setSelectedTodo(td);
-    }
-  };
+	useEffect(() => {
+		dispatch(fetchTodos());
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	},[]);
 
-  const todoDetail = useMemo(() => {
-    return selectedTodo ? (
-      <TodoDetail title={selectedTodo.title} content={selectedTodo.content} />
-    ) : null;
-  }, [selectedTodo]);
+	const navigate = useNavigate()
 
-  return (
-    <div className="TodoList">
-      <div className="title">{title}</div>
-      <div className="todos">
-        {todos.map((td) => {
-          return (
-            <Todo
-              key={`${td.id}_todo`}
-              title={td.title}
-              done={td.done}
-              clicked={() => clickTodoHandler(td)}
-            />
-          );
-        })}
-        {todoDetail}
-        <NavLink to="/new-todo" >New Todo</NavLink>
-      </div>
-    </div>
-  );
+	const clickTodoHandler = (td: TodoType) => {
+		navigate('/todos/' + td.id)
+	};
+
+	const todoDetail = useMemo(() => {
+		return selectedTodo ? (
+			<TodoDetail />
+		) : null;
+	}, [selectedTodo]);
+
+	return (
+		<div className="TodoList">
+			<div className="title">{title}</div>
+			<div className="todos">
+				{todoState.todos.map((td) => {
+					return (
+						<Todo
+							title={td.title}
+							done={td.done}
+							clickDetail={() => clickTodoHandler(td)}
+							clickDone={() => dispatch(toggleDone(td.id))}
+							clickDelete={() => dispatch(deleteTodo(td.id))}
+						/>
+					);
+				})}
+				{todoDetail}
+				<NavLink to="/new-todo" >New Todo</NavLink>
+			</div>
+		</div>
+	);
 }
