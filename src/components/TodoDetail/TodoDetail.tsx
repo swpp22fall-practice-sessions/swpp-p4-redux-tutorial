dd7447a44bf52fcfe84bb4b31d4e60a310379cf3
Yujin Im <jina0605@gmail.com>
2022-09-28 19:54:24 +0900

    Implement TodoDetail

--- src/components/TodoDetail/TodoDetail.tsx
+++ src/components/TodoDetail/TodoDetail.tsx
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
 import "./TodoDetail.css";
+import { AppDispatch } from "../../store";
+import { selectTodo, todoActions } from "../../store/slices/todo";
 
 type Props = {
   title: string;
@@ -6,15 +11,23 @@ type Props = {
 };
 
 const TodoDetail = (props: Props) => {
+  const { id } = useParams();
+  const dispatch = useDispatch<AppDispatch>();
+  const todoState = useSelector(selectTodo);
+
+  useEffect(() => {
+    dispatch(todoActions.getTodo({ targetId: Number(id) }));
+  }, [dispatch, id]);
+
   return (
     <div className="TodoDetail">
       <div className="row">
         <div className="left">Name:</div>
-        <div className="right">{props.title}</div>
+        <div className="right">{todoState.selectedTodo?.title}</div>
       </div>
       <div className="row">
         <div className="left">Content:</div>
-        <div className="right">{props.content}</div>
+        <div className="right">{todoState.selectedTodo?.content}</div>
       </div>
     </div>
   );
