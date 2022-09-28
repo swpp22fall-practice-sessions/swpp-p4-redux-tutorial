b7001214b8b9eb2edda5045ead1ad9e76c245a6d
dongjoocha <dongjoocha@naver.com>
2022-09-28 21:04:12 +0900

    implement lab4

--- src/containers/TodoList/TodoList.tsx
+++ src/containers/TodoList/TodoList.tsx
@@ -1,8 +1,18 @@
-import { useMemo, useState } from "react";
-import { NavLink } from "react-router-dom";
-import Todo from "../../components/Todo/Todo";
-import TodoDetail from "../../components/TodoDetail/TodoDetail";
-import "./TodoList.css";
+import { useEffect, useMemo, useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import Todo from '../../components/Todo/Todo';
+import TodoDetail from '../../components/TodoDetail/TodoDetail';
+import './TodoList.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchTodos,
+  selectTodo,
+  deleteTodo,
+  toggleDone,
+  todoActions,
+} from '../../store/slices/todo';
+import axios from 'axios';
+import { AppDispatch } from '../../store';
 
 interface IProps {
   title: string;
@@ -12,44 +22,75 @@ type TodoType = { id: number; title: string; content: string; done: boolean };
 
 export default function TodoList(props: IProps) {
   const { title } = props;
-  const [selectedTodo, setSelectedTodo] = useState<TodoType | null>(null);
+  // const [selectedTodo, setSelectedTodo] = useState<TodoType | null>(null);
 
-  const [todos, setTodos] = useState<TodoType[]>([
-    { id: 1, title: "SWPP", content: "take swpp class", done: true },
-    { id: 2, title: "Movie", content: "watch movie", done: false },
-    { id: 3, title: "Dinner", content: "eat dinner", done: false },
-  ]);
+  // const [todos, setTodos] = useState<TodoType[]>([
+  //   { id: 1, title: "SWPP", content: "take swpp class", done: true },
+  //   { id: 2, title: "Movie", content: "watch movie", done: false },
+  //   { id: 3, title: "Dinner", content: "eat dinner", done: false },
+  // ]);
+
+  const todoState = useSelector(selectTodo);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get('/api/todo/')
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  });
+
+  useEffect(() => {
+    axios
+      .get('/api/todoerror')
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  });
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, []);
+
+  // const clickTodoHandler = (td: TodoType) => {
+  //   if (selectedTodo === td) {
+  //     setSelectedTodo(null);
+  //   } else {
+  //     setSelectedTodo(td);
+  //   }
+  // };
 
   const clickTodoHandler = (td: TodoType) => {
-    if (selectedTodo === td) {
-      setSelectedTodo(null);
-    } else {
-      setSelectedTodo(td);
-    }
+    navigate('/todos/' + td.id);
   };
 
-  const todoDetail = useMemo(() => {
-    return selectedTodo ? (
-      <TodoDetail title={selectedTodo.title} content={selectedTodo.content} />
-    ) : null;
-  }, [selectedTodo]);
+  // const todoDetail = useMemo(() => {
+  //   return selectedTodo ? (
+  //     <TodoDetail title={selectedTodo.title} content={selectedTodo.content} />
+  //   ) : null;
+  // }, [selectedTodo]);
 
   return (
     <div className="TodoList">
       <div className="title">{title}</div>
       <div className="todos">
-        {todos.map((td) => {
+        {todoState.todos.map((td) => {
           return (
             <Todo
               key={`${td.id}_todo`}
               title={td.title}
               done={td.done}
-              clicked={() => clickTodoHandler(td)}
+              clickDetail={() => clickTodoHandler(td)}
+              clickDone={() => {
+                dispatch(toggleDone(td.id));
+              }}
+              clickDelete={() => dispatch(deleteTodo(td.id))}
             />
           );
         })}
-        {todoDetail}
-        <NavLink to="/new-todo" >New Todo</NavLink>
+        {/* {todoDetail} */}
+        <NavLink to="/new-todo">New Todo</NavLink>
       </div>
     </div>
   );
