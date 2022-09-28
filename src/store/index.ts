002a8322235388b4fa02fbc30b83b5172d42ed39
Yujin Im <jina0605@gmail.com>
2022-09-28 19:38:07 +0900

    configure store with todoReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./slices/todo";
 
 export const store = configureStore({
-  reducer: (state = {}, action) => state,
+  reducer: { todo: todoReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
