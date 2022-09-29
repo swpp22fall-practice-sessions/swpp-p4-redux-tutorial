37c10e06286b19ed9ab23fa523eb216c984be667
gkgkgkgkgk73 <gkgkgkgkgk73@gmail.com>
2022-09-29 20:46:28 +0900

    4

---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./slices/todo";
+
+export const store = configureStore(
+ { reducer: {
+    todo: todoReducer,
+ },
+}); // TODO
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
